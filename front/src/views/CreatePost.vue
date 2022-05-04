b80ef30de6d378ae20410c0d855da3cb73540baf
snow.sr <joaofelipi877@gmail.com>
2022-05-04 16:20:30 -0300

    CreateKisk Now with article default false

--- front/src/views/CreatePost.vue
+++ front/src/views/CreatePost.vue
@@ -47,7 +47,7 @@ export default {};
           type="submit"
           class="w-full px-6 py-2.5 bg-green_ifc text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         >
-          Enviar 
+          Enviar
         </button>
       </form>
     </div>
