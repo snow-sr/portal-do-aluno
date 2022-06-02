5585d48addcd858389c6bc23fe5a4898bb36e0bd
viniihds <viniciushsilva2210@gmail.com>
2022-06-02 09:28:06 -0300

    Correcting merge @snow-sr

--- front/src/components/Feed.vue
+++ front/src/components/Feed.vue
@@ -3,7 +3,7 @@ import Post from "@/components/Post.vue";
 import CreatePost from "../views/CreatePost.vue";
 import CreateKisk from "../components/CreateKisk.vue";
 export default {
-  components: { Post, CreatePost, CreateKisk },
+  components: { Post },
 };
 </script>
 
@@ -21,15 +21,9 @@ export default {
         Criar Kisk
       </button>
 
-      <div
-        class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-        id="staticBackdrop"
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabindex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-      >
+      <div class="modal fade fixed top-0 left-0 hidden w-1/3 h-full outline-none overflow-x-hidden overflow-y-auto"
+        id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog relative w-auto pointer-events-none">
           <div
             class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current"
